ba046dd2e155787eac75cd2377c2fca3a3fed9ef
gmenke54 <menke.gw@gmail.com>
2022-03-02 11:44:11 -0800

    added conditional styling to the active selection

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,19 @@
 <template>
   <div v-if="this.students">
-    <div>Our Students</div>
-    <div>
-      <div class="nut">{{this.title.display}}</div>
-      <DoughnutChart :chartData="edData" :options="this.options" />
-    </div>
-    <div class="btn-bar">
-      <div class="btn" @click="this.title={display: 'Middle Initial', data: 'initial'}">Middle Initial</div>
-      <div class="btn" @click="this.title={display: 'Last Name', data: 'l_name'}">Last Name</div>
-      <div class="btn" @click="this.title={display: 'Prefix', data: 'prefix'}">Prefix</div>
-      <div class="btn" @click="this.title={display: 'State', data: 'state'}">State</div>
-      <div class="btn" @click="this.title={display: 'Zip Code', data: 'zip'}">Zip Code</div>
-      <div class="btn" @click="this.title={display: 'Education Level', data: 'education'}">Education Level</div>
-      <div class="btn" @click="this.title={display: 'Income Level', data: 'income'}">Income Level</div>
+    <div class="title">Students</div>
+    <div class="flex-row">
+      <div class="btn-bar">
+        <div class="btn" @click="this.title={display: 'Education Level', data: 'education'}, setActiv('education')" :class="education">Education Level</div>
+        <div class="btn" @click="this.title={display: 'Income Level', data: 'income'}, setActiv('income')" :class="income">Income Level</div>
+        <div class="btn" @click="this.title={display: 'State', data: 'state'}, setActiv('state')" :class="state">State</div>
+        <div class="btn" @click="this.title={display: 'Prefix', data: 'prefix'}, setActiv('prefix')" :class="prefix">Prefix</div>
+        <div class="btn" @click="this.title={display: 'Last Name', data: 'l_name'}, setActiv('l_name')" :class="l_name">Last Name</div>
+        <div class="btn" @click="this.title={display: 'Middle Initial', data: 'initial'}, setActiv('initial')" :class="initial">Middle Initial</div>
+      </div>
+      <div class="nut">
+        <div class="hole">{{this.title.display}}</div>
+        <DoughnutChart :chartData="edData" :options="this.options" />
+      </div>
     </div>
   </div>
 </template>
@@ -38,13 +39,66 @@ export default {
           }
         }
       },
-      students: null
+      students: null,
+      activ: 'education',
+      initial: 'grey',
+      l_name: 'grey',
+      prefix: 'grey',
+      state: 'grey',
+      income: 'grey',
+      education: 'black',
     }
   },
   methods: {
     async getStudents(){
       const res = await axios.get('http://localhost:3000/students/')
       this.students = res.data
+    },
+    setActiv(level){
+      this.activ = level
+      if (level === 'initial'){
+        this.initial='black'
+        this.l_name='grey'
+        this.prefix='grey'
+        this.state='grey'
+        this.income='grey'
+        this.education='grey'
+      } else if (level === 'l_name'){
+        this.initial='grey'
+        this.l_name='black'
+        this.prefix='grey'
+        this.state='grey'
+        this.income='grey'
+        this.education='grey'
+      } else if (level === 'prefix'){
+        this.initial='grey'
+        this.l_name='grey'
+        this.prefix='black'
+        this.state='grey'
+        this.income='grey'
+        this.education='grey'
+      } else if (level === 'state'){
+        this.initial='grey'
+        this.l_name='grey'
+        this.prefix='grey'
+        this.state='black'
+        this.income='grey'
+        this.education='grey'
+      } else if (level === 'income'){
+        this.initial='grey'
+        this.l_name='grey'
+        this.prefix='grey'
+        this.state='grey'
+        this.income='black'
+        this.education='grey'
+      } else {
+        this.initial='grey'
+        this.l_name='grey'
+        this.prefix='grey'
+        this.state='grey'
+        this.income='grey'
+        this.education='black'
+      }
     }
   },
   computed: {
@@ -92,7 +146,51 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  color: #4F5451;
+  font-weight: 700;
+  font-size: 25px
+}
+.nut{
+  position: relative;
+  display: flex;
+  justify-content: center;
+  margin: 10px;
+}
+.hole{
+  position: absolute;
+  top: 46.5%;
+  text-align: center;
+}
+.btn{
+  cursor: pointer;
+}
+.btn:hover{
+  color: black
+}
+.btn-bar{
+  width: 225px;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  border-left: 3px solid black;
+  padding-left: 5px;
+  padding-top: 7px;
+  padding-bottom: 6px;
+  margin-bottom: 15px;
+}
+.flex-row{
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  flex-wrap: wrap
+}
+.title{
+  margin: 15px;
+  color: black;
+  font-size: 30px;
+}
+.black{
+  color: black
 }
 </style>
